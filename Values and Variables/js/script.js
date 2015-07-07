bf7c5972a0820b05fea4175fa8563dfabdbcdcb2
Sean Walsh <spwalsh@fullsail.edu>
2015-07-07 08:55:52 -0400

    Values and Variables
    Values and Variables Lecture Completed

---
+++ Values and Variables/js/script.js
@@ -0,0 +1,32 @@
+
+ /* Sean Walsh
+SDI Section 01
+Values and Variables
+ */
+
+
+ //alert("See, this works.");
+
+ /*
+var yearBorn = 1955;
+ console.log("Hello World!");
+ console.log(yearBorn);
+*/
+// alert(yearBorn);
+/*
+ var age = 40; //Number Variable
+ //age = 40;
+
+ //var yearOfBirth = 1970;
+
+var isStudent = false; //Boolean Variable
+ var sean = "my friend"; //String Variable
+ var firstName = "Sean";
+*/
+//Arrays
+ var muppetNames= ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0, 1, 2, 3
+ //Changes index #1 of the array.
+ muppetNames[4] = "Fozzy";
+ muppetNames [5]= "Animal";
+ muppetNames [6]= "Beaker";
+ console.log (muppetNames);
