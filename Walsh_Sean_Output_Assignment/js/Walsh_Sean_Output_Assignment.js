c6f0daf1ba26923fb788a17adaa7d5dd8d3f17f1
Sean Walsh <spwalsh@fullsail.edu>
2015-07-08 19:30:45 -0400

    number variable
    prompted user for information, then casted response as a number
    variable. Displayed content to the user.

--- Walsh_Sean_Output_Assignment/js/Walsh_Sean_Output_Assignment.js
+++ Walsh_Sean_Output_Assignment/js/Walsh_Sean_Output_Assignment.js
@@ -10,14 +10,18 @@ Sean Walsh
 //Engage the user.
 
 //variables
-var user = prompt("What is thy name, Sir?");//ask his or her name.
+var user              =  prompt("What is thy name, Sir?");//ask his or her name.
+//letting the user assign a value for Dragon Root.
+var dragonRoot        =  prompt("Please enter the number of Dragon Root you have found on your journey below.");
 
 
-
-
-
+//casting the ingredients as numbers.
+dragonRoot     =  Number(dragonRoot);//Changed variable to a number
 
 
 
 //Outputs.
-console.log("Thy name is "+user+".");
+console.log ("Welcome, "+user+" to\n \"The House of Potions.\"");//Outputting name.
+console.log (user+", you have found "+dragonRoot+" Dragon Root.");//outputs number of dragon root as a number.
+
+
