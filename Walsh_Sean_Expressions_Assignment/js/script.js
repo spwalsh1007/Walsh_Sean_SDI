0c7b2e6ef89e50b84e2ca7ec7f78e1d901e36d97
Sean Walsh <spwalsh@fullsail.edu>
2015-07-14 09:41:38 -0400

    added array
    added array and meaningful outputs

--- Walsh_Sean_Expressions_Assignment/js/script.js
+++ Walsh_Sean_Expressions_Assignment/js/script.js
@@ -6,35 +6,43 @@
  */
 
 
-alert("See, yes. Works this file does.");
+//alert("See, yes. Works this file does.");
 
 
 //variables
 var         customerName;
-
+var         chocolatePrice;
+var         vanillaPrice;
+var         strawberryPrice;
+var         scoopTotal;
 
 
 
 //arrays
+var         flavors = ["chocolate", "vanilla", "strawberry"];
 
 
 
+//Alerts and Prompts
+customerName  = prompt("Welcome to Flips Ice Cream vending machine. \n What's your name, friend?");
+alert                 ("Today we have chocolate, vanilla, and strawberry ice cream at $1.25 per scoop.\n Please press \"ok\" to begin your order.");
+flavors[0]    = prompt("How many scoops of chocolate ice cream would you like?");
+flavors[1]    = prompt("How many scoops of vanilla ice cream would you like?");
+flavors[2]    = prompt("How many scoops of strawberry ice cream would you like?");
 
-//prompts
-customerName  = prompt("Welcome to Flips Ice Cream Truck. \n What's your name, friend?");
-
-
-
-
-//casting the numbers
-
-
+//casting the strings to numbers.
+flavors[0]  = Number(flavors[0]);
+flavors[1]  = Number(flavors[1]);
+flavors[2]  = Number(flavors[2]);
 
 
 
 
 //math
-
+chocolatePrice  = flavors[0] * 1.25;
+vanillaPrice    = flavors[1] * 1.25;
+strawberryPrice = flavors[2] * 1.25;
+scoopTotal      = flavors[0] + flavors[1] + flavors[2];
 
 
 
@@ -42,3 +50,7 @@ customerName  = prompt("Welcome to Flips Ice Cream Truck. \n What's your name, f
 
 //outputs
 console.log("Welcome "+customerName+", to Flip's.");
+console.log("That's "+flavors[0]+" Scoops of chocolate ice cream for $"+chocolatePrice+".");
+console.log("That's "+flavors[1]+" Scoops of vanilla ice cream for $"+vanillaPrice+".");
+console.log("That's "+flavors[2]+" Scoops of strawberry ice cream for $"+strawberryPrice+".");
+console.log("That's "+scoopTotal+" scoops of ice cream");
