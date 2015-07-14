64d4c161c4a377940744d9a0196ba303b6b618d1
Sean Walsh <spwalsh@fullsail.edu>
2015-07-14 07:42:08 -0400

    expressions setup
    Setup expressions assignment files

---
+++ Walsh_Sean_Expressions_Assignment/js/script.js
@@ -0,0 +1,30 @@
+/*
+  Sean Walsh
+  SDI 1507 Section 01
+  Expressions Assignment
+  7/14/15
+ */
+
+
+alert("See, yes. Works this file does.");
+
+
+//variables
+
+
+
+
+
+//prompts
+
+
+
+
+//math
+
+
+
+
+
+
+//outputs
