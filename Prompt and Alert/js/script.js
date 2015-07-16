5eb2a6d54ff77f73755d6ded7407a2f4d2abb6f1
Sean Walsh <spwalsh@fullsail.edu>
2015-07-16 08:01:52 -0400

    prompt and alert
    prompt and alert assignment completed.

---
+++ Prompt and Alert/js/script.js
@@ -0,0 +1,19 @@
+/*
+Sean Walsh
+SDI 1507 Section 01
+Prompt and alert
+7/16/15
+ */
+
+//alert("Holy codebreaker, Batman, This really works.");
+/*
+var userInput = prompt("Enter your year of birth");
+console.log(userInput);
+*/
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
+var height  = prompt("We are calculating the area of a rectangle. \nPlease enter the length");
+var area = width * height;
+console.log(area);
+var  result  = "The area of your rectangle is " +area+" square feet.";
+alert (result);
