9ec11c036b18252cd1cb5e66711a4b7a945158ef
Sean Walsh <spwalsh@fullsail.edu>
2015-07-16 07:35:52 -0400

    Screencast Expresions
    Screencast Expressions completed.

---
+++ Screencast_Expressions/js/script.js
@@ -0,0 +1,78 @@
+/*
+  Created by seanwalsh on 7/16/15.
+  SDI 1507 Section 01
+  ScreenCast: Expressions
+ */
+
+
+//alert("Now, this works ok.");
+
+/*var yearBorn = 1974
+var age = 2015 - yearBorn - 1;
+console.log(age);
+*/
+
+//area of a triangle is half the width times the height.
+/*var width       = 8;
+var height      = 7;
+var area        = width * height/2;
+console.log(area);
+*/
+
+/*var remainder = 211 % 2;
+console.log(remainder);
+*/
+/*
+var quiz1   = 87;
+var quiz2   = 100;
+var quiz3   = 60;
+var quiz4   = 80;
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4) /4;
+//console.log(average);
+
+var width       = 7;
+var length      = 6;
+var perimiter   = length * 2 + width * 2;
+console.log(perimiter);
+*/
+/*
+var orangeBins = [234, 567, 883];
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
+*/
+/*
+var firstName = "Sean";
+var lastName  = "Walsh";
+var fullName  = firstName + " " + lastName;
+console.log(fullName);
+*/
+/*
+var a = 6;
+var b = 7;
+var result = a + b;
+console.log (result);
+*/
+
+/*
+var stringVar = "6";
+var result = 7 + Number(stringVar);
+console.log(result);
+*/
+
+/*
+var areaCode = 407;
+var firstPart = 203;
+var secondPart = 4529;
+//(407) 203-4529
+var phoneNumber = "(" + String(areaCode) + ")" + String(firstPart) +"-" + String(secondPart);
+console.log(phoneNumber);
+*/
+
+//assignment operators.
+
+var a = 3;
+a --;
+console.log(a);
+
+
